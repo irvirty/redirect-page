21b05154f23a5f82a3c6c0d0832a7a4ae7a27ca1
irvirty <115393328+irvirty@users.noreply.github.com>
2024-07-09 06:47:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Redirect page v.1.0.0
+
+let lRedirectUsername= "irvirty";
+
+let lRedirectTime = 10000;
+let lRedirectUrl = "https://" + lRedirectUsername + ".pages.dev";
+
+
+setTimeout(function(){
+location.href = lRedirectUrl; 
+}, lRedirectTime); 
+
+
+document.getElementById("lRedirectTime").innerHTML = "Redirection after " + lRedirectTime / 1000 + " seconds.";
+document.getElementById("lRedirectUrl").innerHTML = `<span class="green">New address</span>:<br> <a class="brand" href="${lRedirectUrl}">${lRedirectUrl}</a>`;
